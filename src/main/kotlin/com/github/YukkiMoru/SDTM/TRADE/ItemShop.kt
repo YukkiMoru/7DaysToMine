ce468ebc835dcff9367b4f22b5c70abcb67accd6
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-11 14:38:16 +0900

    add : 村人の追加

--- src/main/kotlin/com/github/YukkiMoru/SDTM/TRADE/ItemShop.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/TRADE/ItemShop.kt
@@ -3,12 +3,14 @@ package com.github.YukkiMoru.SDTM.TRADE
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Villager
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.MerchantRecipe
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
+import java.io.File
 
 class ItemShop(private val plugin: JavaPlugin) {
 
@@ -23,20 +25,27 @@ class ItemShop(private val plugin: JavaPlugin) {
                 villager.villagerLevel = 2
                 villager.villagerType = Villager.Type.PLAINS
                 villager.isCustomNameVisible = true
-                villager.customName = "test"
+                villager.customName = "Weapon Master"
                 villager.isPersistent = true
 
-                // Create custom trade
-                val buyItem = ItemStack(Material.IRON_INGOT, 10)
-                val sellItem = ItemStack(Material.BEDROCK, 19)
-                val meta = sellItem.itemMeta
-                meta?.setDisplayName("a")
-                meta?.lore = listOf("a")
-                sellItem.itemMeta = meta
+                // Create trades
+                val recipes = mutableListOf<MerchantRecipe>()
 
-                val recipe = MerchantRecipe(sellItem, 9999999)
-                recipe.addIngredient(buyItem)
-                villager.recipes = listOf(recipe)
+                // Trade 1: 10 iron blocks for 4 emerald blocks
+                val buyItem1 = ItemStack(Material.IRON_BLOCK, 10)
+                val sellItem1 = ItemStack(Material.EMERALD_BLOCK, 4)
+                val recipe1 = MerchantRecipe(sellItem1, 9999999)
+                recipe1.addIngredient(buyItem1)
+                recipes.add(recipe1)
+
+                // Trade 2: 2 diamond blocks for 20 emerald blocks
+                val buyItem2 = ItemStack(Material.DIAMOND_BLOCK, 2)
+                val sellItem2 = ItemStack(Material.EMERALD_BLOCK, 20)
+                val recipe2 = MerchantRecipe(sellItem2, 9999999)
+                recipe2.addIngredient(buyItem2)
+                recipes.add(recipe2)
+
+                villager.recipes = recipes
             }
         }.runTask(plugin)
     }
