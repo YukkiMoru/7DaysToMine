04a11d98dbedfd24f2a0a0880d31e249a0b78e7d
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-12 18:06:03 +0900

    add : ピッケルの破壊対象の物体だと破壊できるようになる

--- src/main/kotlin/com/github/YukkiMoru/SDTM/WORLD/RegenerateOres.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/WORLD/RegenerateOres.kt
@@ -3,16 +3,13 @@ package com.github.YukkiMoru.SDTM.WORLD
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
-import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 
 class RegenerateOres(private val plugin: JavaPlugin) : Listener {
-
 
     init {
         // Schedule a repeating task to update the target block display
@@ -25,8 +22,22 @@ class RegenerateOres(private val plugin: JavaPlugin) : Listener {
                     } else {
                         player.sendActionBar("No target block")
                     }
+                    val itemInHand: ItemStack = player.inventory.itemInMainHand
+                    if (itemInHand.type == Material.IRON_PICKAXE && itemInHand.itemMeta?.isUnbreakable == true) {
+                        val container = itemInHand.itemMeta?.persistentDataContainer
+                        val key = NamespacedKey(plugin, "destroyable_blocks")
+                        val destroyableBlocks = container?.get(key, PersistentDataType.STRING)?.split(",") ?: emptyList<String>()
+                        val targetBlock = player.getTargetBlockExact(5) // Get the block the player is looking at within 5 blocks
+                        if (targetBlock != null && destroyableBlocks.contains(targetBlock.type.key.toString())) {
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute ${player.name} player.block_break_speed base set 1")
+                        } else {
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute ${player.name} player.block_break_speed base set 0")
+                        }
+                    } else {
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "attribute ${player.name} player.block_break_speed base set 0")
+                    }
                 }
             }
-        }.runTaskTimer(plugin, 0L, 1L) // Run every second (20 ticks)
+        }.runTaskTimer(plugin, 0L, 20L) // Run every second (20 ticks)
     }
 }
