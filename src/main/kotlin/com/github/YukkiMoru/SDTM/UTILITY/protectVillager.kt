f8d9f6e47dd743f01aeae8585085536cffaabc04
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-12 10:02:55 +0900

    WIP add : 村人を襲わないようにするプログラム

--- src/main/kotlin/com/github/YukkiMoru/SDTM/UTILITY/protectVillager.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/UTILITY/protectVillager.kt
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class protectVillager(private val plugin: JavaPlugin) : Listener {
@@ -17,4 +18,15 @@ class protectVillager(private val plugin: JavaPlugin) : Listener {
 	  event.isCancelled = true
 	}
   }
+
+  //村人を襲わない(ただしラグが見込まれるので禁止)
+//	@EventHandler
+//	fun onEntityTargetLivingEntity(event: EntityTargetLivingEntityEvent) {
+//		val target = event.target
+//		val entity = event.entity
+//
+//		if (target?.type == EntityType.VILLAGER && entity.type == EntityType.ZOMBIE) {
+//			event.isCancelled = true
+//		}
+//	}
 }
