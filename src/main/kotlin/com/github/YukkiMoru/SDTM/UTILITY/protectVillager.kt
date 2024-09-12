6c74e24cea4398d82d9e607181db4455357e17d5
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-12 10:26:21 +0900

    enable : 村人を襲わないようにするプログラム

--- src/main/kotlin/com/github/YukkiMoru/SDTM/UTILITY/protectVillager.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/UTILITY/protectVillager.kt
@@ -9,24 +9,25 @@ import org.bukkit.event.entity.EntityTargetLivingEntityEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class protectVillager(private val plugin: JavaPlugin) : Listener {
-  @EventHandler
-  fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
-	val entity = event.entity
-	val damager = event.damager
+//村人を攻撃させない
+	@EventHandler
+	fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
+		val entity = event.entity
+		val damager = event.damager
 
-	if (entity.type == EntityType.VILLAGER && damager is Player) {
-	  event.isCancelled = true
+		if (entity.type == EntityType.VILLAGER && damager is Player) {
+		  event.isCancelled = true
+		}
 	}
-  }
 
-  //村人を襲わない(ただしラグが見込まれるので禁止)
-//	@EventHandler
-//	fun onEntityTargetLivingEntity(event: EntityTargetLivingEntityEvent) {
-//		val target = event.target
-//		val entity = event.entity
-//
-//		if (target?.type == EntityType.VILLAGER && entity.type == EntityType.ZOMBIE) {
-//			event.isCancelled = true
-//		}
-//	}
+//村人を襲わないようにする
+	@EventHandler
+	fun onEntityTargetLivingEntity(event: EntityTargetLivingEntityEvent) {
+		val target = event.target
+		val entity = event.entity
+
+		if (target?.type == EntityType.VILLAGER && entity.type == EntityType.ZOMBIE) {
+			event.isCancelled = true
+		}
+	}
 }
