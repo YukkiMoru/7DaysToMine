26d48abae496e1204e3eff46d962a18861d90dc2
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-13 11:46:16 +0900

    fix : 一部のコードをコメントアウト

--- src/main/kotlin/com/github/YukkiMoru/SDTM/WORLD/RegenerateOres.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/WORLD/RegenerateOres.kt
@@ -19,80 +19,86 @@ import java.util.*
 
 class RegenerateOres(private val plugin: JavaPlugin) : Listener {
 
-    private val playerTargetBlocks = mutableMapOf<UUID, Material?>()
+	private val playerTargetBlocks = mutableMapOf<UUID, Material?>()
 
-    init {
-        // Schedule a repeating task to update the target block display
-        object : BukkitRunnable() {
-            override fun run() {
-                for (player in Bukkit.getOnlinePlayers()) {
-                    val targetBlock = player.getTargetBlockExact(5) // Get the block the player is looking at within 5 blocks
-                    val previousBlock = playerTargetBlocks[player.uniqueId]
+	init {
+		// Schedule a repeating task to update the target block display
+		object : BukkitRunnable() {
+			override fun run() {
+				for (player in Bukkit.getOnlinePlayers()) {
+					val targetBlock = player.getTargetBlockExact(5)
+					val previousBlock = playerTargetBlocks[player.uniqueId]
 
-                    if (targetBlock != null && targetBlock.type != Material.AIR) {
-                        player.sendActionBar("Target Block: ${targetBlock.type}")
-                    } else {
-                        player.sendActionBar("No target block")
-                    }
+					if (targetBlock != null && targetBlock.type != Material.AIR) {
+//						player.sendActionBar("Target Block: ${targetBlock.type}")
+						player.sendActionBar(previousBlock?.name ?: "No previous block")
+					} else {
+//						player.sendActionBar("No target block")
+						player.sendActionBar(previousBlock?.name ?: "No previous block")
+					}
 
-                    if (targetBlock?.type != previousBlock) {
-                        playerTargetBlocks[player.uniqueId] = targetBlock?.type
-                        checkAndSetBlockBreakSpeed(player, targetBlock?.type)
-                    }
-                }
-            }
-        }.runTaskTimer(plugin, 0L, 1L) // Run every 1 ticks (0.05 seconds)
-    }
 
-    @EventHandler
-    fun onItemHeld(event: PlayerItemHeldEvent) {
-        val player = event.player
-        val targetBlock = player.getTargetBlockExact(5)
-        // wait 1 tick
-        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-            checkAndSetBlockBreakSpeed(player, targetBlock?.type)
-        }, 1L)
-    }
+					if (targetBlock?.type != previousBlock) {
+						playerTargetBlocks[player.uniqueId] = targetBlock?.type
+						checkAndSetBlockBreakSpeed(player, targetBlock?.type)
+					}
+				}
+			}
+		}.runTaskTimer(plugin, 0L, 1L) // Run every 1 ticks (0.05 seconds)
+	}
 
-    @EventHandler
-    fun onSwapHandItems(event: PlayerSwapHandItemsEvent) {
-        val player = event.player
-        val targetBlock = player.getTargetBlockExact(5)
-        // wait 1 tick
-        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-            checkAndSetBlockBreakSpeed(player, targetBlock?.type)
-        }, 1L)
-    }
+	@EventHandler
+	fun onItemHeld(event: PlayerItemHeldEvent) {
+		val player = event.player
+		val targetBlock = player.getTargetBlockExact(5)
+		// wait 1 tick
+		Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+			checkAndSetBlockBreakSpeed(player, targetBlock?.type)
+		}, 1L)
+	}
 
-    private fun checkAndSetBlockBreakSpeed(player: Player, targetBlockType: Material?) {
-        val itemInHand: ItemStack = player.inventory.itemInMainHand
-        val itemInOffHand: ItemStack = player.inventory.itemInOffHand
-        val key = NamespacedKey(plugin, "destroyable_blocks")
+	@EventHandler
+	fun onSwapHandItems(event: PlayerSwapHandItemsEvent) {
+		val player = event.player
+		val targetBlock = player.getTargetBlockExact(5)
+		// wait 1 tick
+		Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+			checkAndSetBlockBreakSpeed(player, targetBlock?.type)
+		}, 1L)
+	}
 
-        if (isDestroyable(itemInHand, key, targetBlockType) || isDestroyable(itemInOffHand, key, targetBlockType)) {
-            setBlockBreakSpeed(player, 1.0)
-            //message to player
-            player.sendMessage("You can break this block!")
-        } else {
-            setBlockBreakSpeed(player, 0.0)
-            //message to player
-            player.sendMessage("You can't break this block!")
-        }
-    }
+	private fun checkAndSetBlockBreakSpeed(player: Player, targetBlockType: Material?) {
+		val itemInHand: ItemStack = player.inventory.itemInMainHand
+		val itemInOffHand: ItemStack = player.inventory.itemInOffHand
+		val key = NamespacedKey(plugin, "destroyable_blocks")
 
-    private fun isDestroyable(item: ItemStack, key: NamespacedKey, targetBlockType: Material?): Boolean {
-        if (item.type == Material.IRON_PICKAXE && item.itemMeta?.isUnbreakable == true) {
-            val container = item.itemMeta?.persistentDataContainer
-            val destroyableBlocks = container?.get(key, PersistentDataType.STRING)?.split(",") ?: emptyList<String>()
-            return targetBlockType != null && destroyableBlocks.contains(targetBlockType.key.toString())
-        }
-        return false
-    }
+		if (isDestroyable(itemInHand, key, targetBlockType) || isDestroyable(itemInOffHand, key, targetBlockType)) {
+			setBlockBreakSpeed(player, 1.0)
+			//message to player
+//			player.sendMessage("You can break this block!")
+		} else {
+			setBlockBreakSpeed(player, 0.0)
+			//message to player
+//			player.sendMessage("You can't break this block!")
+		}
+	}
 
-    private fun setBlockBreakSpeed(player: Player, speed: Double) {
-        val attribute = player.getAttribute(Attribute.PLAYER_BLOCK_BREAK_SPEED)
-        attribute?.modifiers?.forEach { attribute.removeModifier(it) }
-        val modifier = AttributeModifier(NamespacedKey(plugin, "block_break_speed"), speed, Operation.ADD_NUMBER)
-        attribute?.addModifier(modifier)
-    }
+	private fun isDestroyable(item: ItemStack, key: NamespacedKey, targetBlockType: Material?): Boolean {
+		if (item.type == Material.IRON_PICKAXE && item.itemMeta?.isUnbreakable == true) {
+			val container = item.itemMeta?.persistentDataContainer
+			val destroyableBlocks = container?.get(key, PersistentDataType.STRING)?.split(",") ?: emptyList<String>()
+			return targetBlockType != null && destroyableBlocks.contains(targetBlockType.key.toString())
+		}
+		return false
+	}
+
+	private fun setBlockBreakSpeed(player: Player, speed: Double) {
+		val attribute = player.getAttribute(Attribute.PLAYER_BLOCK_BREAK_SPEED)
+		attribute?.modifiers?.forEach { attribute.removeModifier(it) }
+		val modifier = AttributeModifier(NamespacedKey(plugin, "block_break_speed"), speed, Operation.ADD_NUMBER)
+		attribute?.addModifier(modifier)
+
+		//message to player
+//		player.sendMessage("Block break speed: $speed")
+	}
 }
