b578ea5efb07ee64bda8fb9caa88ffebe1093034
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-19 00:17:58 +0900

    WIP add : コードの簡潔化

--- src/main/kotlin/com/github/YukkiMoru/SDTM/WORLD/RegenerateBlocks.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/WORLD/RegenerateBlocks.kt
@@ -1,7 +1,6 @@
 package com.github.YukkiMoru.SDTM.WORLD
 
 import org.bukkit.Material
-import org.bukkit.block.BlockState
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
@@ -13,14 +12,13 @@ class RegenerateBlocks(private val plugin: JavaPlugin) : Listener {
 	@EventHandler
 	fun onBlockBreak(event: BlockBreakEvent) {
 		val block = event.block
-		val originalType = block.type
+		val originalState = block.state
+		val temporaryState = block.state
 
 		val ores = listOf(
 			Material.COAL_ORE,
 			Material.IRON_ORE,
 			Material.DEEPSLATE_IRON_ORE,
-			Material.RED_STAINED_GLASS,
-			Material.RED_STAINED_GLASS_PANE
 		)
 		val gemstones = listOf(
 			Material.RED_STAINED_GLASS
@@ -30,25 +28,26 @@ class RegenerateBlocks(private val plugin: JavaPlugin) : Listener {
 		)
 		val breakableBlocks = ores + gemstones + shardGemstones
 
-		if (breakableBlocks.contains(originalType)) {
-			val temporaryType = when {
-				ores.contains(originalType) -> Material.BEDROCK
-				gemstones.contains(originalType) -> Material.GRAY_STAINED_GLASS
-				shardGemstones.contains(originalType) -> Material.GRAY_STAINED_GLASS_PANE
+		if (breakableBlocks.contains(originalState.type)) {
+			temporaryState.type = when {
+				ores.contains(originalState.type) -> Material.BEDROCK
+				gemstones.contains(originalState.type) -> Material.GRAY_STAINED_GLASS
+				shardGemstones.contains(originalState.type) -> Material.GRAY_STAINED_GLASS_PANE
 				else -> Material.BEDROCK
 			}
-			val originalState: BlockState = block.state
+
+
 			object : BukkitRunnable() {
 				override fun run() {
-					block.type = temporaryType
+					block.type = temporaryState.type
+					temporaryState.update(true, true)
 				}
 			}.runTaskLater(plugin, 1L)
 
-			// Schedule a task to restore the original block after 3 seconds (60 ticks)
 			object : BukkitRunnable() {
 				override fun run() {
-					block.type = originalType
-					originalState.update(true, true) // Restore the original state and force update
+					block.type = originalState.type
+					originalState.update(true, true)
 				}
 			}.runTaskLater(plugin, 60L)
 		}
