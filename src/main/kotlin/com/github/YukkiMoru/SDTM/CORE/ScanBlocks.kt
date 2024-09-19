94a9a01d9b9ce5b7ed5a0cff139f7d19a88d1c3d
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-19 16:15:04 +0900

    add : scanでjsonに保存

--- src/main/kotlin/com/github/YukkiMoru/SDTM/CORE/ScanBlocks.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/CORE/ScanBlocks.kt
@@ -1,10 +1,15 @@
 package com.github.YukkiMoru.SDTM.CORE
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class ScanBlocks {
+
+	private val spawnerLocations = mutableListOf<Map<String, Int>>()
 
 	fun scan(plugin: JavaPlugin, x: Int, y: Int, z: Int) {
 		val world = plugin.server.getWorld("world") ?: return
@@ -17,9 +22,18 @@ class ScanBlocks {
 					val block = loc.block
 					if (block.type == Material.LIGHT) {
 						plugin.logger.info("Spawner found at: ${loc.blockX}, ${loc.blockY}, ${loc.blockZ}")
+						spawnerLocations.add(mapOf("x" to loc.blockX, "y" to loc.blockY, "z" to loc.blockZ))
 					}
 				}
 			}
 		}
+		saveToJson()
+	}
+
+	private fun saveToJson() {
+		val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+		val json = gson.toJson(spawnerLocations)
+		val file = File("plugins/7DaysToMine/spawner_locations.json")
+		file.writeText(json)
 	}
 }
