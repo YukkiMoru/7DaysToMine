e20f1544c4683c95fec374af86cb6f1c2685e84b
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-19 14:27:30 +0900

    add : 逆取引の実装

--- src/main/kotlin/com/github/YukkiMoru/SDTM/TRADE/VillagerPickaxe.kt
+++ src/main/kotlin/com/github/YukkiMoru/SDTM/TRADE/VillagerPickaxe.kt
@@ -76,6 +76,38 @@ class VillagerPickaxe(private val plugin: JavaPlugin) {
 				recipe2.addIngredient(buyItem2)
 				recipes.add(recipe2)
 
+
+// Trade 3: 1 unbreakable iron pickaxe for 10 emeralds
+				val buyItem3 = ItemStack(Material.IRON_PICKAXE, 1)
+				val meta3: ItemMeta = buyItem3.itemMeta
+
+// Set the item to be unbreakable
+				meta3.isUnbreakable = true
+
+// Set the item to be able to break specific blocks using PersistentDataContainer
+				val container3 = meta3.persistentDataContainer
+				val key3 = NamespacedKey(plugin, "destroyable_blocks")
+				val destroyableBlocks3 =
+					"minecraft:coal_ore:1.0:1.0,minecraft:iron_ore:1.0:1.0,minecraft:deepslate_iron_ore:0.8:1.5,minecraft:red_stained_glass:0.5:1.0,minecraft:red_stained_glass_pane:0.7:0.5"
+				container3.set(key3, PersistentDataType.STRING, destroyableBlocks3)
+
+// Add lore to display destroyable blocks
+				val lore3 = listOf(
+					"${ChatColor.GREEN}破壊可能なブロック:",
+					"${ChatColor.GREEN}⛏1.0 ☘1.0 石炭鉱石",
+					"${ChatColor.GREEN}⛏1.0 ☘1.0 鉄鉱石",
+					"${ChatColor.GREEN}⛏0.8 ☘1.5 深層鉄鉱石",
+					"${ChatColor.GREEN}⛏0.5(0.7) ☘1.0(0.5) ルビー鉱石",
+				)
+				meta3.lore = lore3
+
+				buyItem3.itemMeta = meta3
+				val sellItem3 = ItemStack(Material.EMERALD, 10)
+
+				val recipe3 = MerchantRecipe(sellItem3, 9999999)
+				recipe3.addIngredient(buyItem3)
+				recipes.add(recipe3)
+
 				villager.recipes = recipes
 			}
 		}.runTask(plugin)
