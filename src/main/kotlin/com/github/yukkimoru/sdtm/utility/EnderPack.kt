cdbbee5c8eb3a26a78b32b541f69241a83227040
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-22 17:27:33 +0900

    fix : OpenEnderChest()の()の削除

--- src/main/kotlin/com/github/yukkimoru/sdtm/utility/EnderPack.kt
+++ src/main/kotlin/com/github/yukkimoru/sdtm/utility/EnderPack.kt
@@ -7,18 +7,18 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 
 class EnderPack {
-    class OpenEnderChest() : Listener {
+	class OpenEnderChest : Listener {
 
-        @EventHandler
-        fun onPlayerInteract(event: PlayerInteractEvent) {
-            val player = event.player
-            val itemInHand = player.inventory.itemInMainHand
+		@EventHandler
+		fun onPlayerInteract(event: PlayerInteractEvent) {
+			val player = event.player
+			val itemInHand = player.inventory.itemInMainHand
 
-            // 右クリックかつメインハンドでの操作のみを対象とする
-            if (event.hand == EquipmentSlot.HAND && itemInHand.type == Material.RECOVERY_COMPASS) {
-                player.openInventory(player.enderChest)
-                event.isCancelled = true // イベントをキャンセルして通常の動作を防ぐ
-            }
-        }
-    }
+			// 右クリックかつメインハンドでの操作のみを対象とする
+			if (event.hand == EquipmentSlot.HAND && itemInHand.type == Material.RECOVERY_COMPASS) {
+				player.openInventory(player.enderChest)
+				event.isCancelled = true // イベントをキャンセルして通常の動作を防ぐ
+			}
+		}
+	}
 }
