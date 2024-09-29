98f1729e8ace77f4688ab5519ec25946eabc63da
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-09-29 21:39:27 +0900

    fix : ２段ジャンプブーツを身につけたとき、つけてない時の挙動

--- src/main/kotlin/com/github/yukkimoru/sdtm/utility/ability/DoubleJump.kt
+++ src/main/kotlin/com/github/yukkimoru/sdtm/utility/ability/DoubleJump.kt
@@ -1,12 +1,11 @@
 package com.github.yukkimoru.sdtm.utility.ability
 
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
 import com.github.yukkimoru.sdtm.world.FactoryItem
 import org.bukkit.GameMode
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerItemHeldEvent
-import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerToggleFlightEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
@@ -19,6 +18,7 @@ class DoubleJump(private val plugin: JavaPlugin) : Listener {
 	private val flyingPlayers = mutableSetOf<UUID>()
 	private val lastJumpTime = mutableMapOf<UUID, Long>()
 	private val cooldownTime = 3000 // 3 seconds
+	private var wearArmor: Boolean = false
 
 	// なにも装備していない(null), または301のカスタムモデルデータを持つアイテムを装備しているか
 	private fun isWearingDoubleJumperBoots(player: Player): Boolean {
@@ -28,64 +28,64 @@ class DoubleJump(private val plugin: JavaPlugin) : Listener {
 
 	@EventHandler
 	fun onPlayerToggleFlight(event: PlayerToggleFlightEvent) {
-		val player = event.player
+		if (wearArmor) {
+			val player = event.player
+			if (player.gameMode == GameMode.CREATIVE) return
+			event.isCancelled = true
 
-		if (!isWearingDoubleJumperBoots(player)) {
-			player.allowFlight = false
-			player.sendMessage("§cYou can't double jump!")
-			return
-		}
+			val currentTime = System.currentTimeMillis()
+			val playerUUID = player.uniqueId
 
-		if (player.gameMode == GameMode.CREATIVE) return
-
-		event.isCancelled = true
-
-		val currentTime = System.currentTimeMillis()
-		val playerUUID = player.uniqueId
-
-		if (doubleJumpPlayers.contains(player.name)) {
-			//↓使うとクールダウン中に２段ジャンプすると落下ダメージを食らう
+			if (doubleJumpPlayers.contains(player.name)) {
+				//↓使うとクールダウン中に２段ジャンプすると落下ダメージを食らう
 //			player.allowFlight = false
-			player.sendMessage("§aDouble Jump is on cooldown!")
-		} else {
-			player.sendMessage("§cDouble Jump")
-			val lastJump = lastJumpTime[playerUUID] ?: 0
-			if (currentTime - lastJump >= cooldownTime) {
-				player.velocity = player.location.direction.multiply(1.0).setY(1)
-				doubleJumpPlayers.add(player.name)
-				lastJumpTime[playerUUID] = currentTime
+				player.sendMessage("§aDouble Jump is on cooldown!")
+			} else {
+				player.sendMessage("§cDouble Jump")
+				val lastJump = lastJumpTime[playerUUID] ?: 0
+				if (currentTime - lastJump >= cooldownTime) {
+					player.velocity = player.location.direction.multiply(1.0).setY(1)
+					doubleJumpPlayers.add(player.name)
+					lastJumpTime[playerUUID] = currentTime
 
-				object : BukkitRunnable() {
-					override fun run() {
-						player.allowFlight = true
-						player.world.playSound(player.location, "entity.wither.shoot", 0.05f, 0.1f)
-						player.sendMessage("§aDouble Jump is ready!")
-						doubleJumpPlayers.remove(player.name)
-					}
-				}.runTaskLater(plugin, (cooldownTime / 50).toLong()) // Convert milliseconds to ticks
+					object : BukkitRunnable() {
+						override fun run() {
+							if (wearArmor) {
+								player.allowFlight = true
+								player.world.playSound(player.location, "entity.wither.shoot", 0.05f, 0.1f)
+								player.sendMessage("§aDouble Jump is ready!")
+								doubleJumpPlayers.remove(player.name)
+							}
+						}
+					}.runTaskLater(plugin, (cooldownTime / 50).toLong()) // Convert milliseconds to ticks
+				}
 			}
 		}
 	}
 
-	@EventHandler
-	fun onPlayerJoin(event: PlayerJoinEvent) {
-		val player = event.player
-		if (flyingPlayers.contains(player.uniqueId)) {
-			player.allowFlight = true
-		}
-	}
+//	@EventHandler
+//	fun onPlayerJoin(event: PlayerJoinEvent) {
+//		val player = event.player
+//		if (flyingPlayers.contains(player.uniqueId)) {
+//			player.allowFlight = true
+//		}
+//	}
 
 	@EventHandler
-	fun onPlayerItemHeld(event: PlayerItemHeldEvent) {
+	fun onPlayerArmorChange(event: PlayerArmorChangeEvent) {
 		val player = event.player
-		val boots = player.inventory.boots
 
-		if (boots == null || !FactoryItem(plugin).isItemWithCustomModelData(boots, 301)) {
-			player.allowFlight = false
-			doubleJumpPlayers.remove(player.name)
-		} else {
-			player.allowFlight = true
-			player.sendMessage("§aYou can now double jump!")
-		}
+		object : BukkitRunnable() {
+			override fun run() {
+				wearArmor = isWearingDoubleJumperBoots(player)
+				if (wearArmor) {
+					player.sendMessage("§aYou are wearing Double Jumper Boots by equip!")
+					player.allowFlight = true
+				} else {
+					player.sendMessage("§cYou are not wearing Double Jumper Boots by equip!")
+					player.allowFlight = false
+				}
+			}
+		}.runTaskLater(plugin, 1L) // Delay by 1 tick
 	}
 }
