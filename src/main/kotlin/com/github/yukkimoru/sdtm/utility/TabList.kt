63a84739ff4fc114103e682cbca419bd2f9b1ba3
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-10-03 17:53:33 +0900

    WIP debug : ポーションのクールダウンの廃止予定のものを排除

--- src/main/kotlin/com/github/yukkimoru/sdtm/utility/TabList.kt
+++ src/main/kotlin/com/github/yukkimoru/sdtm/utility/TabList.kt
@@ -1,27 +1,42 @@
 package com.github.yukkimoru.sdtm.utility
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 class TabList {
 
 	fun initialize() {
-		setTabListHeaderAndFooter("§bWelcome to the §c7§aDays§9To§eMine§b!", "§bEnjoy your stay!")
+		setTabListHeaderAndFooter(
+			Component.text("Welcome to the 7DaysToMine!", NamedTextColor.AQUA),
+			Component.text("Enjoy your stay!", NamedTextColor.AQUA)
+		)
 	}
 
-	private fun setTabListHeaderAndFooter(header: String, footer: String) {
+	private fun setTabListHeaderAndFooter(header: Component, footer: Component) {
 		for (player in Bukkit.getOnlinePlayers()) {
-			player.setPlayerListHeaderFooter(header, footer)
+			player.sendPlayerListHeader(header)
+			player.sendPlayerListFooter(footer)
 		}
 	}
 
 	fun updatePotionCooldowns(player: Player, cooldowns: Map<Int, Int>) {
-		val cooldownText = buildString {
-			append("§bCooldowns: ")
-			for ((potionID, timeLeft) in cooldowns) {
-				append("§7P$potionID: §c$timeLeft ")
-			}
-		}
-		player.setPlayerListName("${player.name} $cooldownText")
+		val cooldownText = Component.text("Cooldowns: ", NamedTextColor.AQUA)
+			.decoration(TextDecoration.BOLD, true)
+			.append(
+				Component.text(
+					cooldowns.entries.joinToString(" ") { (potionID, timeLeft) ->
+						"P$potionID: $timeLeft"
+					},
+					NamedTextColor.GRAY
+				)
+			)
+
+		val playerName = Component.text(player.name, NamedTextColor.WHITE)
+		val newPlayerListName = playerName.append(Component.text(" ")).append(cooldownText)
+
+		player.playerListName(newPlayerListName)
 	}
 }
